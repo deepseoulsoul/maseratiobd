ccca0e70b85a8323851713e7878dc1b17dbf5433
deepseoul <djdeevy@gmail.com>
2025-11-01 03:52:20 +0900

    Initial Commit

---
+++ maseratiobd/maseratiobdApp.swift
@@ -0,0 +1,17 @@
+//
+//  maseratiobdApp.swift
+//  maseratiobd
+//
+//  Created by Jin Shin on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct maseratiobdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ maseratiobdTests/maseratiobdTests.swift
@@ -0,0 +1,17 @@
+//
+//  maseratiobdTests.swift
+//  maseratiobdTests
+//
+//  Created by Jin Shin on 11/1/25.
+//
+
+import Testing
+@testable import maseratiobd
+
+struct maseratiobdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
